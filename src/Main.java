6cf887ec63bf887c13a60b744646b0ccec35575d
martinrumisek <mrumisek@seznam.cz>
2023-01-15 21:54:04 +0100

    Ve třídě Main, je spuštěna metody, pro výpis ze souboru, zapis do souboru, přidání dva nové zákazníky, odebrání jednoho zákazníka a výpis souboru do konzole. Vyřešeny vlastní vyjímky.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EvidenceZakaznku ev = new EvidenceZakaznku();
+        try {
+            ev.vypisZeSouboru("text.txt", ":");
+        } catch (MyException e) {
+            System.err.println("Musíte zvolit správný soubor. " + e.getLocalizedMessage());
+        }
+        for (Zakaznik zakaznik: ev.getList()){
+            System.out.println(zakaznik.getJmeno() + " " + zakaznik.getPocetProdeju() );
+
+        }
+        ev.pridej(new Zakaznik("Karel Josef", 20));
+        ev.pridej(new Zakaznik("Tomáš Dědek", 39));
+        ev.odeber(1);
+        try {
+            ev.zmenSoubor("text.txt", ":");
+        } catch (MyException e) {
+            System.err.println("Musíte zvolit správný soubor. " + e.getLocalizedMessage());
+        }
     }
 }

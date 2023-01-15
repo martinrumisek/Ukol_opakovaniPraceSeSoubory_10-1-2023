1ed884d7f5df40a1aac8e7b5294e46426f30dd01
martinrumisek <mrumisek@seznam.cz>
2023-01-15 21:56:43 +0100

    Vytvořeny dva atributy (jmeno, pocetProdeju), jeden konstruktor, getter a setter.

--- src/Zakaznik.java
+++ src/Zakaznik.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Zakaznik {
+import java.io.IOException;
+
+public class Zakaznik {
+    private String jmeno;
+    private int pocetProdeju;
+
+    public Zakaznik(String jmeno, int pocetProdeju) {
+        this.jmeno = jmeno;
+        this.pocetProdeju = pocetProdeju;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public int getPocetProdeju() {
+        return pocetProdeju;
+    }
+
+    public void setPocetProdeju(int pocetProdeju) {
+        this.pocetProdeju = pocetProdeju;
+    }
+
+    public void zvysPocetProdeju(int pocet){
+        try{
+            if (pocet <= 0){
+                throw new IOException();
+            }else {
+                pocetProdeju =+ pocet;
+            }
+        }catch (IOException e){
+            System.err.println("Nemužete počet prodejů zvýšit o záporné číslo");
+        }
+    }
 }
